2d5a0ac2bd6a25e00ac7bc7947b27deb361fc072
VolkanB <112424491+VolB07@users.noreply.github.com>
2025-05-06 16:51:03 +0300

    AppDbContext ve ConnectionStrings eklendi

---
+++ KutuphaneOtomasyonu/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace KutuphaneOtomasyonu.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        // Buraya DbSet<T> tanımlarını ekleyebilirsin
+        // public DbSet<KendiModelin> ModelAdi { get; set; }
+    }
+}
